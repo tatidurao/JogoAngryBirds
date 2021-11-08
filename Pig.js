fd6e9116dbabfedf01bea2cf062aec3e931064a2
tatidurao <91564423+tatidurao@users.noreply.github.com>
2021-11-08 15:56:41 -0300

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,16 @@
+class Pig extends BaseClass {
+  constructor(x,y){
+      super(x,y, 50, 50);
+     this.image = loadImage("sprites/enemy.png");
+  }
+ 
+  display(){
+      
+    super.display();
+  }
+ }
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# JogoAngryBirds
+# angryBirdsStage1
+Stage 1 Angry Birds
